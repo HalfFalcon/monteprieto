13569a1fac6b9f5492616581e9fd932ce0815fb5
malcomkring <malcolm.kring@gmail.com>
2021-01-12 20:34:39 +0000

    trying stuff

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/mp'),
+      dir: require('path').join(__dirname, './coverage/monteprieto'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mp'`, () => {
+  it(`should have as title 'monteprieto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mp');
+    expect(app.title).toEqual('monteprieto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('monteprieto app is running!');
   });
 });
